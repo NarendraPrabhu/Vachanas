0168d97fbd2febf7351a8d69cdf3b2c6d908784f
nprabhu <nprabhu@vera.com>
2018-02-26 21:46:53 +0530

    intermediate

---
+++ app/src/main/java/com/vachanasaahitya/vachanas/ui/VachanaFragment.java
@@ -0,0 +1,10 @@
+package com.vachanasaahitya.vachanas.ui;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by narensmac on 26/02/18.
+ */
+
+public class VachanaFragment extends DialogFragment {
+}
---
+++ app/src/main/java/com/vachanasaahitya/vachanas/ui/VachanasActivity.java
@@ -0,0 +1,19 @@
+package com.vachanasaahitya.vachanas.ui;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by narensmac on 26/02/18.
+ */
+
+public class VachanasActivity extends ListActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
